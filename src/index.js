69cce6b13e2ea004ad941eb6a163b6e1d2334eea
Laevus92 <laevus@yandex.ru>
2023-01-27 19:46:12 +0300

    resolve task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    
+  let result = [];
+    
+  if (matrix == null || matrix === '') {
+      result;
+  } else {
+      for (let i = 1; i < matrix.length; i = i + 2) {
+      matrix[i].sort((x, y) => y-x);
+  }
+  result = matrix.flat();
+  }
+  
+return result;
 }
+
